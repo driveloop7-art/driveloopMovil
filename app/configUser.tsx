891fd2135c8f94689186549638b299d33747055f
Juan Velez <aztvelez@gmail.com>
2026-03-13 20:25:21 -0500

    cambios en la vista de informacion de usuario y se agrego el icono para regresar a la vista anterior

--- app/configUser.tsx
+++ app/configUser.tsx
@@ -1,49 +1,60 @@
+import { router } from "expo-router";
 import React from "react";
-import { ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import CustomButton from "../components/CustomButton";
 
 export default function ProfileInfo() {
   return (
-    <ScrollView className="flex-1 bg-gray-100 px-5 pt-10">
-      {/* Título */}
-      <Text className="text-2xl font-roboto-bold text-center mb-8">
-        Información de perfil
-      </Text>
-
-      {/* Nombre */}
-      <View className="border border-red-400 rounded-xl p-4 mb-4 bg-white">
-        <Text className="text-center font-roboto-bold mb-1">Nombre</Text>
-        <Text className="text-center text-gray-700">Juan Camilo</Text>
+    <ScrollView className="flex-1 px-5 pt-10">
+      <View className="flex-row  justify-center">
+        <TouchableOpacity
+          onPress={() => router.back()}
+          className="absolute left-0"
+        >
+          <Image
+            source={require("../assets/images/icon-regre.png")}
+            style={{ width: 60, height: 30 }}
+          />
+        </TouchableOpacity>
+        <Text className="text-2xl font-roboto-bold text-center mb-8">
+          Información de perfil
+        </Text>
       </View>
 
-      {/* Apellido */}
-      <View className="border border-red-400 rounded-xl p-4 mb-4 bg-white">
-        <Text className="text-center font-roboto-bold mb-1">Apellido</Text>
-        <Text className="text-center text-gray-700">Gonzalez Giraldo</Text>
-      </View>
-
-      {/* Teléfono */}
-      <View className="border border-red-400 rounded-xl p-4 mb-4 bg-white">
-        <Text className="text-center font-roboto-bold mb-2">Teléfono</Text>
-        <Text className="text-center text-gray-700 mb-3">+57 305 8191471</Text>
-
-        <View className="border-t border-gray-300 pt-3">
-          <CustomButton title="MODIFICAR" />
+      <View className="flex-1 justify-center">
+        <View className="mt-24 border border-red-500 rounded-xl p-4 mb-4 bg-white">
+          <Text className="text-center font-roboto-bold mb-1">Nombre</Text>
+          <Text className="text-center text-gray-700">Juan Camilo</Text>
         </View>
-      </View>
 
-      {/* Correo */}
-      <View className="border border-red-400 rounded-xl p-4 mb-10 bg-white">
-        <Text className="text-center font-roboto-bold mb-2">
-          Correo electrónico
-        </Text>
+        <View className="border border-red-500 rounded-xl p-4 mb-4 bg-white">
+          <Text className="text-center font-roboto-bold mb-1">Apellido</Text>
+          <Text className="text-center text-gray-700">Gonzalez Giraldo</Text>
+        </View>
 
-        <Text className="text-center text-gray-700 mb-3">
-          juanvelez@gmail.com
-        </Text>
+        <View className="mt-14 border border-red-500 rounded-xl p-4 mb-4 bg-white">
+          <Text className="text-center font-roboto-bold mb-2">Teléfono</Text>
+          <Text className="text-center text-gray-700 mb-3">
+            +57 305 8191471
+          </Text>
 
-        <View className="border-t border-gray-300 pt-3">
-          <CustomButton title="MODIFICAR" />
+          <View className="border-t border-gray-300 pt-3">
+            <CustomButton title="MODIFICAR" />
+          </View>
+        </View>
+
+        <View className="border border-red-500 rounded-xl p-4 mb-10 bg-white">
+          <Text className="text-center font-roboto-bold mb-2">
+            Correo electrónico
+          </Text>
+
+          <Text className="text-center text-gray-700 mb-3">
+            juanvelez@gmail.com
+          </Text>
+
+          <View className="border-t border-gray-300 pt-3">
+            <CustomButton title="MODIFICAR" />
+          </View>
         </View>
       </View>
     </ScrollView>
