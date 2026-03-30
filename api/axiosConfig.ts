ba06bada711aed71888ffac21ba0072be644a884
Dinomoli <Dinomoli1606@gmail.com>
2026-03-29 21:14:00 -0500

    net

--- api/axiosConfig.ts
+++ api/axiosConfig.ts
@@ -3,7 +3,7 @@ import * as SecureStore from 'expo-secure-store';
 
 // Configuramos la instancia de axios
 const api = axios.create({
-    baseURL: 'http://192.168.20.94:8000/api',
+    baseURL: 'https://driveloop.ddns.net/api',
     // Lo dejo aqui para ajustarlo con la url real de la api
     headers: {
         'Content-Type': 'application/json',
